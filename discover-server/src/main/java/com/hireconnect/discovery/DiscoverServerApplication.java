4838279c1a332901c0a5dca4b73b12d9acc23bfd
ALAN-KRATI <alankratisaxena@gmail.com>
2026-04-02 15:49:17 +0530

    Set up initial infrastructure

---
+++ discover-server/src/main/java/com/hireconnect/discovery/DiscoverServerApplication.java
@@ -0,0 +1,14 @@
+package com.hireconnect.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoverServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverServerApplication.class, args);
+	}
+
+}
